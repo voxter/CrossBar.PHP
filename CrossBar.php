6626e738383102145d8684388e21f3e3eb40ef0a
Daniel Finke <danielfinke2011@gmail.com>
2015-01-07 19:38:58 -0800

    Added trial methods for auth error handling

--- CrossBar.php
+++ CrossBar.php
@@ -374,6 +374,29 @@ class CrossBar {
 
 
 	}
+	
+	function getHandleFailure( $type, $id = null, $filters = array(), $account_id = null, $handler = null) {
+		//$this->log(__CLASS__."->".__METHOD__." is DEPRECATED, please replace with ".__CLASS__."->get_object");
+
+		if( $account_id == null ) $account_id = $this->use_account_id;
+
+		$filter = '';
+
+		if( count($filters) ) {
+			foreach( $filters as $key => $val ) $filter .= "filter_$key=$val&";
+			if( strlen($filter) ) $filter = '?'.substr($filter,0,-1);
+		} else if( strlen($id) ) {
+			$filter = "/$id";
+		}
+
+		//$this->log("GET /v1/accounts/{$account_id}/$type$filter");
+		$response = $this->sendHandleFailure("GET","/v1/accounts/{$account_id}/$type$filter", $handler);
+
+		return($response['data']);
+
+
+
+	}
 
 
 	function post( $type, $id, $data, $account_id = null ) {
@@ -568,6 +591,10 @@ class CrossBar {
 		return($response[0]);
 	}
 
+	function get_user_by_nameHandleFailure( $username, $account_id = null, $handler = null) {
+		$response = $this->getHandleFailure('users',null,array('username'=>$username),$account_id, $handler);	
+		return($response[0]);
+	}
 
 	function get_user_id( $username ) {
 		$user = $this->get_user($username);
@@ -1414,6 +1441,129 @@ class CrossBar {
 		return json_decode($this->body,true);
 		
 
+	}
+	
+	function sendHandleFailure( $method, $url, $failureHandler, $post_data = NULL, $type = 'application/json', $accept_type = "application/json, application/octet-stream, audio/*, */*") {
+		$bldred=''; 
+		$bldgrn=''; 
+		$bldylw='';
+		$bldblu='';
+		$bldpur='';
+		$bldcyn='';
+		$bldwht='';
+		$txtrst='';
+
+		if($this->color == true) {
+			$bldred=chr(0x1B).'[1;31m';
+			$bldgrn=chr(0x1B).'[1;32m';
+			$bldylw=chr(0x1B).'[1;33m';
+			$bldblu=chr(0x1B).'[1;34m';
+			$bldpur=chr(0x1B).'[1;35m';
+			$bldcyn=chr(0x1B).'[1;36m';
+			$bldwht=chr(0x1B).'[1;37m';
+			$txtrst=chr(0x1B).'[0m'; 
+		}
+
+		// Establish stream to endpoint
+		$mstart = microtime(true);
+		if($this->socket_stream == null) {
+			$this->socket_stream = fsockopen($this->host, $this->port, $errno, $errstr);
+		}
+		
+		// If stream connection failed, return
+		if(!$this->socket_stream) {
+			$this->socket_stream = null;
+			$this->fsock_errno = $errno;
+			$this->fsock_errstr = $errstr;
+			return false; //if the connection fails return false
+		}
+
+		// Connection headers
+		$request = "$method $url HTTP/1.0\r\nHost: $this->host\r\n";
+		if(isset($this->user)) {
+			$request .= "Authorization: Basic ".base64_encode("$this->user:$this->pass")."\r\n";
+		}
+		if($type != null) {
+			$request .= "Content-Type: $type\r\n";
+		}
+		$request .= "Accept: $accept_type\r\n";
+		if(isset($this->xauth)) {
+			$request .= "X-Auth-Token: {$this->xauth}\r\n";
+		}
+
+		// Add post data to request
+		if($post_data) {
+			$request .= "Content-Length: ".strlen($post_data)."\r\n\r\n";
+			$request .= "$post_data\r\n";
+		}
+		else {
+			$request .= "\r\n";
+		}
+
+		// Write request to server
+		fwrite($this->socket_stream, $request);
+		
+		// Get response from server
+		$response = "";
+		while(!feof($this->socket_stream) && $this->socket_stream != null) {
+			$response .= fgets($this->socket_stream);
+		}
+		fclose($this->socket_stream);
+		$this->socket_stream = null;
+
+		// Log connection finished
+		$mend = microtime(true);
+		$this->log("{$bldred}{$method} {$this->host}:{$this->port}$url{$txtrst} {$bldylw}µT:".($mend - $mstart)."{$txtrst}");
+
+		list($this->headers, $this->body) = explode("\r\n\r\n", $response, 2);
+
+		$REQUEST_ID = '';
+		if(strlen($this->headers)) {
+			$hexp = explode("\n",$this->headers);
+			foreach($hexp as $line) {
+				if(strstr($line,"X-Request-ID")) {
+					$reqxp = explode(":",$line);
+					$REQUEST_ID = $reqxp[1];
+				}
+			}
+		}
+
+		// DELETE ends with 204 for success
+		if($method == "DELETE") {
+			if(stristr($this->headers,"204 No Content")) {
+				return array('status' => 'success');
+			}
+		}
+		
+		// Allow the failure handler to correct the credentials and try again
+		if(stristr($this->headers, "401 Unauthorized") && isset($failureHandler)) {
+			$failureHandler->fix();
+			$this->xauth = $failureHandler->newXauth;
+			return $this->sendHandleFailure($method, $url, null, $post_data, $type, $accept_type);
+		}
+
+		// If errors occurred, log them
+		if(!stristr($this->headers, "200 OK") && !stristr($this->headers, "201 Created") ||
+			stristr($this->headers, "401 Unauthorized") || stristr($this->headers, "400 Not Found") ||
+			stristr($this->headers, "500 Internal Server") || stristr($this->headers,"400 Bad Request")) { 
+			$this->log("{$bldpur}>>>>: $method $url HTTP/1.0 ($type) len:".strlen($post_data)."$txtrst \n");
+			if($post_data && $type == 'application/json') {
+				$this->log("{$bldpur}>>>>: ".trim($post_data)."\n");
+			}
+			$this->log("{$bldylw}<<<<: ".trim($hexp[0])." µT=".($mend - $mstart)." request_id:$REQUEST_ID{$txtrst}\n");
+			$this->log("{$bldylw}<<<<: ".$this->headers."{$txtrst}");
+			$this->log("{$bldylw}<<<<: ".$this->body."{$txtrst}");
+			
+			$temp = json_decode($this->body, true);
+			
+			return array('status' => 'failure', 'error' => $temp['error'], 'errors' => $temp['data'], 'message' => $temp['message']);
+		}
+
+		// Return the data from server
+		if($this->force_no_decode) {
+			return $this->body;
+		} 
+		return json_decode($this->body, true);
 	}
 	
 	function sendValidate($method, $url, $post_data = NULL, $type = 'application/json', $accept_type = "application/json, application/octet-stream, audio/*, */*") {
